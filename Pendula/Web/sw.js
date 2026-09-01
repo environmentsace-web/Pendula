a4f19969cc8590a8f6fe7d3c9a5b5075dcd55cb8
environmentsace-web <environmentsace@gmail.com>
2026-09-01 15:06:36 +0200

    Add files via upload

--- Pendula/Web/sw.js
+++ Pendula/Web/sw.js
@@ -3,7 +3,7 @@
    i sam interfejs se otvara i bez mreze. Podaci o zonama se uvijek povlace
    svjezi - stari podaci bi bili gori od nikakvih. */
 
-const KES = "pendula-v1";
+const KES = "pendula-v2";   // podici broj pri svakoj izmjeni interfejsa
 const OKOSNICA = [
   "./",
   "./index.html",
